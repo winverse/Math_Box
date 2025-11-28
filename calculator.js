38335a2ef6a5e22a31d5a0a23ee67d04fc5f51e9
winverse <public.winverse@gmail.com>
2025-11-28 18:33:57 +0900

    Update mutiply function
    calculator.js supports mutiply now

--- calculator.js
+++ calculator.js
@@ -1,7 +1,12 @@
+// (x: number, y: number) -> number
 function add(a, b) {
   return a + b;
 }
 
+// (x: number, y: number) -> number
 function substract(a, b) {
   return a - b;
 }
+
+// (x: number, y: number) -> number
+const mutiply = (a, b) => a * b;
